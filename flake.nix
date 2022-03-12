fc978ce9cf48ba63124382712269f7d599fac725
hugosenari <hugosenari@gmail.com>
2022-03-12 15:29:34 -0300

    chore: re index suffer

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''a nim library for drawing 2d shapes, text, and images to 32bit software pixel buffers'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."suffer-master".url = "path:./master";
+  inputs."suffer-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."suffer-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''a nim library for drawing 2d shapes, text, and images to 32bit software pixel buffers'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-suffer-master.flake = false;
+  inputs.src-suffer-master.owner = "emekoi";
+  inputs.src-suffer-master.ref   = "refs/heads/master";
+  inputs.src-suffer-master.repo  = "suffer";
+  inputs.src-suffer-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-suffer-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-suffer-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
